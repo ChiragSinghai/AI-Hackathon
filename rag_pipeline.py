68627c0bad77ef76759ab20c6647ad8a72196c4e
nrakheja28 <80903881+nrakheja28@users.noreply.github.com>
2026-05-22 15:51:09 +0530

    updatedKey

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -12,7 +12,7 @@ client = httpx.Client(verify=False)
 llm = ChatOpenAI(
     base_url="https://genailab.tcs.in",
     model="azure_ai/genailab-maas-DeepSeek-V3-0324",
-    api_key="sk-LXltlj5p_LKYSHv01Xxsxg",
+    api_key="sk-LXItlj5p_LKYSHv01Xxsxg",
     http_client=client
 )
 
@@ -20,7 +20,7 @@ llm = ChatOpenAI(
 embedding_model = OpenAIEmbeddings(
     base_url="https://genailab.tcs.in",
     model="azure/genailab-maas-text-embedding-3-large",
-    api_key="sk-LXltlj5p_LKYSHv01Xxsxg",
+    api_key="sk-LXItlj5p_LKYSHv01Xxsxg",
     http_client=client
 )
 
